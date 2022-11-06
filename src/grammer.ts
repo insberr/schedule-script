5f93079f226badae503d1af50e7755c1f227ec8a
Jonah Matteson <jonahmatteson@gmail.com>
2022-11-06 07:00:21 +0000

    code moment

--- src/grammer.ts
+++ src/grammer.ts
@@ -1,8 +1,12 @@
-import { readFileSync } from 'fs';
+// import { readFileSync } from 'fs';
 import * as peggy from 'peggy';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import * as pegGrammerFile from 'bundle-text:./grammer.pegjs';
+
 //const start = performance.now()
-const gened = peggy.generate(readFileSync(__dirname + '/grammer.pegjs', 'utf-8'), {
+const gened = peggy.generate(/*readFileSync(__dirname + '/grammer.pegjs', 'utf-8')*/ pegGrammerFile, {
     output: 'parser',
     cache: true,
 });
